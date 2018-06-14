2cd0df212989ca4c8d5b402d1851987f2e6d7625
Trent Robbins <robbintt@gmail.com>
2018-06-13 17:52:44 -0700

    Template App for Phaser 3 initial commit. Retrofit from Phaser 2 phonegap+phaser app, initialized from the npm repository using the phonegap CLI.

---
+++ www/app.js
@@ -0,0 +1,31 @@
+
+var startEvent = "DOMContentLoaded";
+if(window.cordova){
+    startEvent = "deviceready";
+}
+document.addEventListener(startEvent,function() {
+    
+    var config = {
+      type: Phaser.AUTO,
+      width: 800,
+      height: 600,
+      scene: {
+        preload: preload,
+        create: create
+        //update: update
+      }
+    };
+    
+    var game = new Phaser.Game(config);
+
+    function preload () {
+        this.load.image('logo', 'phaser.png');
+    }
+
+    // i got rid of the phaser2 dynamic sprite location etc.
+    // i am sure i will eventually learn how to do that though
+    function create () {
+        var logo = this.add.sprite(400, 300, 'logo');
+    }
+
+});
