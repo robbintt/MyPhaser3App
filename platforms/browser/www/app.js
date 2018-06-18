38f41f85c4fd6c0fdc62876bb6fa8f1ed024179d
Trent Robbins <robbintt@gmail.com>
2018-06-17 23:28:09 -0700

    Removed more possible sources of padding, margin, borderg

--- platforms/browser/www/app.js
+++ platforms/browser/www/app.js
@@ -4,28 +4,5 @@ if(window.cordova){
     startEvent = "deviceready";
 }
 document.addEventListener(startEvent,function() {
-    
-    var config = {
-      type: Phaser.AUTO,
-      width: 375,
-      height: 812,
-      scene: {
-        preload: preload,
-        create: create
-        //update: update
-      }
-    };
-    
-    var game = new Phaser.Game(config);
-
-    function preload () {
-        this.load.image('logo', 'phaser.png');
-    }
-
-    // i got rid of the phaser2 dynamic sprite location etc.
-    // i am sure i will eventually learn how to do that though
-    function create () {
-        var logo = this.add.sprite(100, 75, 'logo');
-    }
 
 });
